d5414a9641baacc11424bcd606b3161cb0c20889
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2023-06-13 19:36:49 -0400

    Add community page

---
+++ src/app/community/page.tsx
@@ -0,0 +1,14 @@
+import Layout from 'components/layouts/default'
+
+export const metadata = {
+    title: "Community",
+    description: "Program Adagrad Community"
+}
+
+export default () => {
+    return (
+        <Layout>
+            "Page"
+        </Layout>
+    )
+}
