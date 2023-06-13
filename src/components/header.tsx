ba1cf7eb713f5d5727a21e6895ef439b45462cbb
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2023-06-13 19:36:02 -0400

    Add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import { link } from "fs"
+
+
+export default () => {
+    return (
+        <header className="flex items-center justify-between p-6 " role="banner">
+            <nav className="flex gap-6">
+                <a href="/blog">Blog</a>
+                <a href="/community">Community</a>
+                <a href="/about">About</a>
+            </nav>
+            <div className="flex items-center gap-6">
+                <a href="/login">Sign in</a>
+                <a href="/register" className="inline-block border rounded py-1 px-3">Sign up</a>
+            </div>
+        </header>
+    )
+}
