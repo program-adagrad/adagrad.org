bbccfc02e10a3787f5794c3bd9f3ed12e3183e20
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2023-06-13 19:36:21 -0400

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Layout from 'components/layouts/default'
+
+export const metadata = {
+    title: "About",
+    description: "About Program Adagrad"
+}
+
+export default () => {
+    return (
+        <Layout>
+            "Page"
+        </Layout>
+    )
+}
