a261e7f716b964e1d6743261e70a7a6a754eada5
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2023-06-13 19:36:31 -0400

    Add blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,14 @@
+import Layout from 'components/layouts/default'
+
+export const metadata = {
+    title: "Blog",
+    description: "Program Adagrad Blog"
+}
+
+export default () => {
+    return (
+        <Layout>
+            "Page"
+        </Layout>
+    )
+}
