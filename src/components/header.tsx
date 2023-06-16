0e4f1935709546227fb27615c3499e91b4a7cc6b
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2023-06-15 20:58:48 -0400

    Edit header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { link } from "fs"
 
 export default () => {
     return (
-        <header className="flex items-center justify-between p-6 " role="banner">
+        <header className="flex items-center justify-between p-6" role="banner">
             <nav className="flex gap-6">
                 <a href="/blog">Blog</a>
                 <a href="/community">Community</a>
@@ -11,7 +11,7 @@ export default () => {
             </nav>
             <div className="flex items-center gap-6">
                 <a href="/login">Sign in</a>
-                <a href="/register" className="inline-block border rounded py-1 px-3">Sign up</a>
+                <a href="/register" className="inline-block border border-black rounded py-1 px-3">Sign up</a>
             </div>
         </header>
     )
