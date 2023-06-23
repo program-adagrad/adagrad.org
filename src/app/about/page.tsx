ac8449c72c65fb64d8cef1148d66fac59bad16e0
Aaron Tian <58406472+waiwasabi@users.noreply.github.com>
2023-06-22 22:33:21 -0400

    Change "about" page hero margin

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default () => {
     return (
         <main>
-            <div className="text-center my-36 w-2/3 m-auto">
+            <div className="text-center my-36 w-4/5 md:w-2/3 m-auto">
                 <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl xl:text-6xl">Toward a new software era.</h1>
                 <h2 className="mt-7 text-black text-opacity-60 text-l md:text-xl lg:text-2xl xl:text-3xl">Program Adagrad is driving a vision of free and inclusive software for everyone. Here's how we do it.</h2>
             </div>
